13fff0b3af5b9fe6a2d6073bc7828a151ba4fac1
mirtorande <mirtorande@gmail.com>
2023-12-18 17:44:46 +0100

    Updated OpenGL Libraries

--- local_libs/include/window_io.h
+++ local_libs/include/window_io.h
@@ -121,7 +121,7 @@ GLFWwindow* initializeWindow()
     // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
